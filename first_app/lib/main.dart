fe2cdc80426f993f1bb2671e42604536ad5fcbf3
mzain7 <mzain.akhtar3@gmail.com>
2023-09-28 21:41:02 +0500

    new

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(const MaterialApp(home:Scaffold(body: Center(child: Text('Hello ,World!')),) ));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Hello ,World!')),
+      ),
+    ),
+  );
 }
